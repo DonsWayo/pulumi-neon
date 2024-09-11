c15b90d1e5436cecf99c4e2ce8850d69d4b7ca4f
Juan Jose Carracedo <34981526+DonsWayo@users.noreply.github.com>
2024-09-12 00:11:02 +0200

    Update examples: Modify NodeJS example to use new resource types

--- examples/nodejs/index.ts
+++ examples/nodejs/index.ts
@@ -1,7 +1,31 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as neon from "@pulumi/neon";
 
-const myRandomResource = new neon.Random("myRandomResource", {length: 24});
-export const output = {
-    value: myRandomResource.result,
-};
+// Get the Neon API key from Pulumi config
+const config = new pulumi.Config();
+//const neonApiKey = config.requireSecret("neonApiKey");
+
+// Configure the Neon provider with the API key
+const neonProvider = new neon.Provider("neonProvider", {
+    apiKey: "c1v9gfm4f2ug1hsacotgaix1ibvrbw57yd0f34msbdnja2zy25tl8qbv52q71w1f",
+});
+
+const project = new neon.Project("MyNeonProject", {
+    name: "MyNeonProject",
+    regionId: "aws-us-west-2",
+}, { provider: neonProvider });
+
+const branch = new neon.Branch("myBranch", {
+    projectId: project.id,
+    name: "main",
+}, { provider: neonProvider });
+
+const database = new neon.Database("myDatabase", {
+    projectId: project.id,
+    branchId: branch.id,
+    name: "mydb",
+}, { provider: neonProvider });
+
+export const neonProjectId = project.id;
+export const neonBranchId = branch.id;
+export const neonDatabaseName = database.name;
